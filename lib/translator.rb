c679da5d563fdb5aa317398c7193e3723ed0e0b3
Sangeetha Rajeev <sang.rajeev@gmail.com>
2018-04-18 03:34:19 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -12,8 +12,8 @@ def load_library(path)
   hash
 end
 
-def get_japanese_emoticon
-  # code goes here
+def get_japanese_emoticon(path, emoticon)
+  load_library(path)
 end
 
 def get_english_meaning
